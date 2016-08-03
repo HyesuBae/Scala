05ffe6fc9284dc5c7b6bd5c6095597836e3b3c0d
HyesuBae <hsb506@gmail.com>
2016-08-03 17:20:22 +0900

    Functional Programming in SCALA (Ch.2)

---
+++ scalaStudy1/src/chapter2_gettingstarted/Exercise.scala
@@ -0,0 +1,55 @@
+package chapter2_gettingstarted
+
+/**
+  * Created by hyesubae on 16. 7. 30.
+  */
+object Exercise {
+  def main(args: Array[String]): Unit = {
+    println(Exercise.fib(6))
+    println(Exercise.fib2(6))
+  }
+
+  // Exercise 2.1  T_T
+  def fib(n: Int) : Int = {
+    def go(n: Int) : Int = {
+      if (n == 0) 0
+      else if (n == 1) 1
+      else go(n-1) + go(n-2)
+    }
+
+    go(n-1)
+  }
+
+  def fib2(n:Int): Int = {
+    def go(n: Int, prev: Int, cur: Int) : Int = {
+      if (n == 1) prev
+      else go(n-1, cur, prev+cur)
+    }
+    go(n, 0, 1)
+  }
+
+  // Exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean ={
+    def loop(n: Int) : Boolean ={
+      if (n >= as.length) true    // 땡! as.length-1 까지 계산해야함. 아래 else if에서 array의 n+1값도 조회하니까.
+      else if(ordered(as(n), as(n+1))) false
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  // Exercise 2.3
+  def curry[A,B,C](f:(A,B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)    // a => b => f(a,b) 로 표현 가능. 타입 추론이 가능하기 때문인가!?
+  }
+
+  // Exercise 2.4
+  def uncurry[A,B,C](f: A => B => C) : (A, B) => C = {
+    (a: A, b: B) => f(a)(b)       // (a,b) => f(a)(b) 로 표현 가
+  }
+
+  // Exercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C ={
+    (a: A) => f(g(a))             // a => f(g(a))로 표현 가능
+  }
+}
--- scalaStudy1/src/chapter2_gettingstarted/MyModule.scala
+++ scalaStudy1/src/chapter2_gettingstarted/MyModule.scala
@@ -7,6 +7,7 @@ object MyModule {
   def main(args: Array[String]): Unit ={
     println(formatResult("absolute", -35, abs))
     println(formatResult("factorial", 6, factorial))
+    println( findFirst(Array(7,9,13), (x: Int) => x == 14))
   }
   def abs(n: Int): Int ={
     if(n < 0) -n
@@ -24,5 +25,19 @@ object MyModule {
   def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d."
     msg.format(name, n, f(n))
+  }
+
+  def findFirst[A](as: Array[A], p:A => Boolean): Int ={
+    @annotation.tailrec
+    def loop(n: Int) : Int = {
+      if(n >= as.length) -1
+      else if(p(as(n))) n
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  def partial1[A,B,C](a: A, f: (A,B)=>C): B => C ={
+    (b: B) => f(a,b)
   }
 }
